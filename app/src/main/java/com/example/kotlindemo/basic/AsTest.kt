b0cac5973251cf68662922bc585a647d253a80cc
zjj <1282450075@qq.com>
2022-03-31 19:29:21 +0800

    add kotlin test file

---
+++ app/src/main/java/com/example/kotlindemo/basic/AsTest.kt
@@ -0,0 +1,27 @@
+package com.example.kotlindemo.basic
+
+/**
+ * name：zjj
+ * date：2022/3/31
+ * desc：类型转换
+ */
+fun main() {
+
+    val a = "1"
+
+    /*// null: 判断String是不是Int类型,显然不是,所以返回null
+    val b : Int? = a as? Int
+    // 1 : Integer.parseInt(a)
+    val c : Int = a.toInt()
+    println("b:$b,c:$c")*/
+
+    /*// elvis 操作符
+    val name : String? = null
+    val length = name?.length ?: name?.length
+    println("length:$length")*/
+
+    // 非空断言
+    var assertString : String? = null
+    var str = assertString!!.length
+    // 报错: NPE
+}
